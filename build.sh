84f186148717158b27b0f9bef0f6db0550ed85ba
djy-git <djyoon0223@gmail.com>
2022-06-04 15:04:24 +0900

    [add] build.sh | Scripts for building `djyoon0223/base:basic`, `djyoon0223/base:full`

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo docker build -t base:basic -f base.basic.Dockerfile .
-sudo docker build -t base:full -f base.full.Dockerfile .
+sudo docker build -t djyoon0223/base:basic -f base.basic.Dockerfile .
+sudo docker build -t djyoon0223/base:full -f base.full.Dockerfile .
