4203ce3638379965fc16369b289b1c8ad07879f8
djy-git <djyoon0223@gmail.com>
2022-06-12 06:05:27 +0900

    [update] context/bin/entrypoint.sh | Add `source ~/.bashrc`

--- context/bin/entrypoint.sh
+++ context/bin/entrypoint.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
+source ~/.bashrc
+
 # Account, bashrc, vim, jupyter setting
 cat /opt/docker/context/setting/account | chpasswd
-printf "\n# PATH \nexport PATH=$PATH" >> /root/.bashrc
 cat /opt/docker/context/setting/bashrc >> /root/.bashrc
 cat /opt/docker/context/setting/vimrc >> /usr/share/vim/vimrc
 jupyter notebook --generate-config && \
 cat /opt/docker/context/jupyter/jupyter_notebook_config.py >> /root/.jupyter/jupyter_notebook_config.py
 
 # Start ssh
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-mkdir -p /run/sshd
+echo "PermitRootLogin yes" >> /etc/ssh/sshd_config && \
+mkdir -p /run/sshd && \
 service ssh start
 
 # Start jupyter
