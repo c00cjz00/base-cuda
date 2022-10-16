93f9e74c09a151ed72c59af7ec95e79d31beb887
djy-git <djyoon0223@gmail.com>
2022-10-17 05:58:35 +0900

    [update] port 추가

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 sudo docker run \
---name compute_server \
---hostname base \
+--name "compute-server" \
+--hostname "3090" \
 --gpus '"device=0"' \
 --ipc host \
 --restart always \
 --privileged \
--v /root/project:/root/project \
+-v /workspace/shared:/workspace \
+-v /workspace2/shared:/workspace2 \
 -p 10022:22 \
 -p 13306:3306 \
 -p 15000:5000 \
@@ -16,9 +17,11 @@ sudo docker run \
 -p 16006:6006 \
 -p 17860:7860 \
 -p 18000:8000 \
+-p 18384:8384 \
 -p 18786:8786 \
 -p 18787:8787 \
 -p 18888:8888 \
--p 18889:8889 \
+-p 22000:22000 \
+-p 10000-10010:10000-10010 \
 -itd \
 djyoon0223/base:full
