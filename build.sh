44ba88f914c8502855b2e20ff81d129d9be37973
alchemine <djyoon0223@gmail.com>
2023-05-01 23:47:11 +0900

    [update] Reorder

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for tag in basic pycaret full tf_torch
+for tag in basic pycaret tf_torch full
 do
   sudo docker build -t djyoon0223/base:$tag -f dockerfile/base.$tag.Dockerfile .
 done
--- push.sh
+++ push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for tag in basic pycaret full tf_torch
+for tag in basic pycaret tf_torch full
 do
   sudo docker push djyoon0223/base:$tag
 done
