5574ae3eacdc922e7e0cd6ca1ff9f8e0af715384
alchemine <djyoon0223@gmail.com>
2023-06-25 19:38:01 +0900

    [update] Add forwarding ports

--- run.sh
+++ run.sh
@@ -8,5 +8,18 @@ sudo docker run \
 --restart always \
 --privileged \
 -v /root/project:/root/project \
+-p 10022:22 \
+-p 13306:3306 \
+-p 15000:5000 \
+-p 15006:5006 \
+-p 16000:6000 \
+-p 16006:6006 \
+-p 17860:7860 \
+-p 18000:8000 \
+-p 18384:8384 \
+-p 18786:8786 \
+-p 18787:8787 \
+-p 18888:8888 \
+-p 18889:8889 \
 -itd \
-djyoon0223/base:full
+djyoon0223/base:basic
