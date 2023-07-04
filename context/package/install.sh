84450dc858a9da7b05d5338155e708052646332b
alchemine <djyoon0223@gmail.com>
2023-07-05 00:06:14 +0900

    [update] context

--- context/package/install.sh
+++ context/package/install.sh
@@ -5,16 +5,6 @@ CONTEXT=/opt/docker/context
 # apt package
 apt update && xargs apt install -y < $CONTEXT/package/requirements.apt
 
-# bashrc
-cat $CONTEXT/package/bashrc >> ~/.bashrc
-
-# vim
-cat $CONTEXT/package/vimrc >> /usr/share/vim/vimrc
-
-# ssh
-echo "PermitRootLogin yes" >> /etc/ssh/sshd_config
-mkdir -p /run/sshd
-
 # python
 ln -s /usr/bin/python3 /usr/bin/python
 apt install -y python3-pip
