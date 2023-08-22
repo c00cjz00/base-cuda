2d4d6b43879fe91b6586b68f812b25e9c1b7332a
alchemine <djyoon0223@gmail.com>
2023-08-22 15:50:42 +0900

    [remove] test scripts

--- context/test/pycaret.py
+++
@@ -1,7 +0,0 @@
-from pycaret.datasets import get_data
-from pycaret.classification import setup
-
-
-def test():
-    data = get_data('diabetes')
-    setup(data, target='Class variable', use_gpu=True)
--- context/test/torch.py
+++
@@ -1,5 +0,0 @@
-import torch
-
-
-def test():
-    assert torch.cuda.is_available(), "There are no GPUs available for PyTorch!"
