0dcdb6279ea9424dc62b35053eeb63c66b13a837
alchemine <djyoon0223@gmail.com>
2023-11-20 14:52:01 +0900

    [update] Jupyter installation

--- context/package/jupyter/apply_config.sh
+++ context/package/jupyter/apply_config.sh
@@ -2,9 +2,6 @@
 
 CONTEXT=/opt/docker/context
 
-# install jupyter
-pip install jupyter notebook==6.4.4
-
 # jupyter configuration
 jupyter notebook --generate-config
 cat $CONTEXT/package/jupyter/jupyter_notebook_config.py >> ~/.jupyter/jupyter_notebook_config.py
