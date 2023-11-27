309ec5c037287e30ba7f054556fac8443bdb8de9
alchemine <djyoon0223@gmail.com>
2023-11-27 18:21:57 +0900

    [update] Update docker configuration

--- run.sh
+++ run.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
 
 sudo docker run \
---name "compute_server" \
+--name "compute" \
 --hostname "dev" \
 --gpus '"device=0"' \
 --ipc host \
 --restart always \
 --privileged \
--v /workspace:/workspace \
--v /data:/data \
+-v .:/opt/project \
 -p 10022:22 \
--p 13306:3306 \
--p 15000:5000 \
--p 15006:5006 \
--p 16000:6000 \
--p 16006:6006 \
--p 17860:7860 \
--p 18000:8000 \
--p 18384:8384 \
--p 18786:8786 \
--p 18787:8787 \
--p 18080:8080 \
--p 18888:8888 \
--p 18889:8889 \
+-p 3306:3306 \
+-p 5000:5000 \
+-p 5006:5006 \
+-p 6000:6000 \
+-p 6006:6006 \
+-p 7860:7860 \
+-p 8000:8000 \
+-p 8080:8080 \
+-p 8384:8384 \
+-p 8501:8501 \
+-p 8786:8786 \
+-p 8787:8787 \
+-p 8888:8888 \
+-p 8889:8889 \
 -itd \
 alchemine/base-cuda:11.8.0-cudnn8-runtime-ubuntu22.04
