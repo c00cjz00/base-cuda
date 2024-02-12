0be9a3c26723c5f38a9388adb9f3d54cc8a05c5b
c00cjz00 <summerhill001@gmail.com>
2024-02-12 12:25:01 +0800

    Update install_python.sh

--- context/utils/install_python.sh
+++ context/utils/install_python.sh
@@ -15,15 +15,15 @@ python3.9  get-pip.py
 python3.10 get-pip.py
 rm get-pip.py
 
-# set python3.8 to default
-update-alternatives --install /usr/bin/python python /usr/bin/python3.8  3
-update-alternatives --install /usr/bin/python python /usr/bin/python3.9  2
-update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
-rm /usr/bin/python3 && ln -s /usr/bin/python /usr/bin/python3
+# set python3.10 to default
+#update-alternatives --install /usr/bin/python python /usr/bin/python3.8  3
+#update-alternatives --install /usr/bin/python python /usr/bin/python3.9  2
+#update-alternatives --install /usr/bin/python python /usr/bin/python3.10 1
+rm /usr/bin/python && rm /usr/bin/python3 && cp /usr/bin/python3.10 /usr/bin/python && ln -s /usr/bin/python /usr/bin/python3 
 
-# set pip3.8 to default
+# set pip3.10 to default
 rm /usr/local/bin/pip
-update-alternatives --install /usr/local/bin/pip pip /usr/local/bin/pip3.8  3
-update-alternatives --install /usr/local/bin/pip pip /usr/local/bin/pip3.9  2
-update-alternatives --install /usr/local/bin/pip pip /usr/local/bin/pip3.10 1
-rm /usr/local/bin/pip3 && ln -s /usr/local/bin/pip /usr/local/bin/pip3
+#update-alternatives --install /usr/local/bin/pip pip /usr/local/bin/pip3.8  3
+#update-alternatives --install /usr/local/bin/pip pip /usr/local/bin/pip3.9  2
+#update-alternatives --install /usr/local/bin/pip pip /usr/local/bin/pip3.10 1
+rm /usr/local/bin/pip && rm /usr/local/bin/pip3 && cp /usr/local/bin/pip3.10 /usr/local/bin/pip  && ln -s /usr/local/bin/pip /usr/local/bin/pip3
